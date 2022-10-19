37d9f75b05208bce5fe6629f1725c40a60954009
elnazbola <elnazb02@gmail.com>
2022-10-19 17:27:23 +0600

    dc-cuource-elnaz

---
+++ examples/WebServer/src/main/java/org/example/Processor.java
@@ -0,0 +1,152 @@
+package org.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+public class Processor {
+    private final Socket socket;
+    private final HttpRequest request;
+
+    public Processor(Socket socket, HttpRequest request) {
+        this.socket = socket;
+        this.request = request;
+    }
+
+    static void prime_number(int number) throws IOException
+    {
+        
+        int a; 
+        int b; 
+        int flag;
+
+        System.out.println(
+                "Prime numbers from 1 and " + number
+                        + " : ");
+
+        for (a = 1; a <= number; a++) {
+            if (a == 1 || a == 0)
+                continue;
+
+            flag = 1;
+
+            for (b = 2; b <= a / 2; ++b) {
+                if (a % b == 0) {
+                    flag = 0;
+                    break;
+                }
+            }
+
+ 
+            if (flag == 1)
+                System.out.print(a + " ");
+        }
+    }
+
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            int z = Integer.parseInt(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    public void process() throws IOException {
+        System.out.println("Got request:");
+        System.out.println(request.toString());
+        System.out.println(request.getRequestLine());
+        System.out.flush();
+
+        PrintWriter output = new PrintWriter(socket.getOutputStream());
+
+        String[] requestSplit = request.getRequestLine().split(" ");
+
+        if (requestSplit[1].equals("/create/itemid")){
+            output.println("HTTP/1.1 200 OK");
+            output.println("Content-Type: text/html; charset=utf-8");
+            output.println();
+            output.println("<html>");
+            output.println("<head><title>hello</title></head>");
+            output.println("<body><p>Create</p></body>");
+            output.println("</html>");
+            output.flush();
+            socket.close();
+        } else if(requestSplit[1].equals("/delete/itemid")){
+            output.println("HTTP/1.1 200 OK");
+            output.println("Content-Type: text/html; charset=utf-8");
+            output.println();
+            output.println("<html>");
+            output.println("<head><title>hello</title></head>");
+            output.println("<body><p>Delete</p></body>");
+            output.println("</html>");
+            output.flush();
+            socket.close();
+        } else if(requestSplit[1].equals("/exec/params")){
+            output.println("HTTP/1.1 200 OK");
+            output.println("Content-Type: text/html; charset=utf-8");
+            output.println();
+            output.println("<html>");
+            output.println("<head><title>hello</title></head>");
+            output.println("<body><p>Execute</p></body>");
+            output.println("</html>");
+            output.flush();
+            socket.close();
+        } else if(isNumeric(requestSplit[1].substring(1))){
+            output.println("HTTP/1.1 200 OK");
+            output.println("Content-Type: text/html; charset=utf-8");
+            output.println();
+            output.println("<html>");
+            output.println("<head><title>hello</title></head>");
+            output.print("<body><p>");
+            int a, b, flag;
+
+           
+            output.print(
+                    "Prime numbers from 1 and " + Integer.parseInt(requestSplit[1].substring(1))
+                            + " : ");
+
+           
+            for (a = 1; a <= Integer.parseInt(requestSplit[1].substring(1)); a++) {
+
+                
+                if (a == 1 || a == 0)
+                    continue;
+
+                
+                flag = 1;
+
+                for (b = 2; b <= a / 2; ++b) {
+                    if (a % b == 0) {
+                        flag = 0;
+                        break;
+                    }
+                }
+
+               
+                if (flag == 1)
+                    output.print(a + " ");
+            }
+          
+            output.println("</p></body>");
+            output.println("</html>");
+            output.flush();
+            socket.close();
+        } else {
+          
+            output.println("HTTP/1.1 200 OK");
+            output.println("Content-Type: text/html; charset=utf-8");
+            output.println();
+            output.println("<html>");
+            output.println("<head><title>Hello</title></head>");
+            output.println("<body><p>Hello, world!</p></body>");
+            output.println("</html>");
+            output.flush();
+            socket.close();
+        }
+    }
+}
